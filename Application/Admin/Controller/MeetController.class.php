6f10703af492cfbbd082fb941daced30c67834fc
sea <shishouhai163@163.com>
2017-04-25 23:20:30 +0800

    sea

--- Application/Admin/Controller/MeetController.class.php
+++ Application/Admin/Controller/MeetController.class.php
@@ -27,7 +27,7 @@ class meetController extends AdminController {
         if(!empty($_key)){
             $map['meet_name']    =   array('like', '%'.(string)$_key.'%');
         }
-        $field='id,meet_name,status,create_time,begin_time,end_time';
+        $field='id,meet_name,status,create_time,begin_time,end_time,is_open_register';
         $list   = $this->lists($this->_model, $map,null,null,$field);
         int_to_string($list);
         $this->assign('_list', $list);
@@ -73,23 +73,32 @@ class meetController extends AdminController {
             if(empty($meet_name)){
                 $this->error('会议名称必填！');
             }
-            $begin_time=I('post.begin_time');
-            if(empty($begin_time)){
-                $this->error('报名开始时间必填！');
-            }
-            $end_time=I('post.end_time');
-            if(empty($end_time)){
-                $this->error('报名结束时间必填！');
-            }
-            if(strtotime($begin_time)>strtotime($end_time)){
-                $this->error('报名开始时间不能大于报名结束时间！');
+            //是否开放注册
+            $is_open_register=I('post.is_open_register');
+            if($is_open_register==1){
+                $begin_time=I('post.begin_time');
+                if(empty($begin_time)){
+                    $this->error('报名开始时间必填！');
+                }
+                $end_time=I('post.end_time');
+                if(empty($end_time)){
+                    $this->error('报名结束时间必填！');
+                }
+                if(strtotime($begin_time)>strtotime($end_time)){
+                    $this->error('报名开始时间不能大于报名结束时间！');
+                }
             }
             $data=[
                 'meet_name'=>$meet_name,
-                'begin_time'=>$begin_time,
-                'end_time'=>$end_time,
-                'status'=>I('post.status')
+                'is_open_register'=>$is_open_register,
+                'status'=>I('post.status'),
             ];
+            if(!empty(I('post.begin_time'))){
+                $data['begin_time']=I('post.begin_time');
+            }
+            if(!empty(I('post.end_time'))){
+                $data['end_time']=I('post.end_time');
+            }
         }
         if(!empty(I('post.hyxz'))){
             $data=[
@@ -146,7 +155,6 @@ class meetController extends AdminController {
            
         } else {
             $this->form();
-            
             $this->meta_title = '新增会议';
             $this->display('edit');
         }
