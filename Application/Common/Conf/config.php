6608c3df9fd75a6c0bddd74c90692285faa776fe
weinan <99470117@qq.com>
2017-05-09 11:12:27 +0800

    nan

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -39,10 +39,10 @@ return array(
 
     /* 数据库配置 */
     'DB_TYPE'   => 'mysqli', // 数据库类型
-    'DB_HOST'   => '60.205.231.42', // 服务器地址
+    'DB_HOST'   => '59.110.228.209', // 服务器地址
     'DB_NAME'   => 'meet', // 数据库名
     'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => 'root',  // 密码
+    'DB_PWD'    => 'tlcc2017',  // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => 'onethink_', // 数据库表前缀
 
